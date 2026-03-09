c7dfe8ee9543bdd8a8e53fb2115dfcd2a1db4c45
kc357174 <kc357174@gmail.com>
2026-03-09 19:36:56 +0530

    Initial commit: Add controller package

---
+++ AI-Interview-Sys/src/main/java/com/example/AI_Interview_Sys/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.AI_Interview_Sys.controller;
+
+import com.example.AI_Interview_Sys.model.User;
+import com.example.AI_Interview_Sys.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+    @RestController
+    @RequestMapping("/auth")
+    @CrossOrigin
+    public class AuthController {
+
+        @Autowired
+        private UserService userService;
+
+        @PostMapping("/register")
+        public User register(@RequestBody User user) {
+            return userService.registerUser(user);
+        }
+
+        @PostMapping("/login")
+        public Object login(@RequestBody User loginUser) {
+
+            Optional<User> user = userService.login(
+                    loginUser.getEmail(),
+                    loginUser.getPassword()
+            );
+
+            if (user.isPresent()) {
+                return user.get();
+            }
+
+            return "Invalid Credentials";
+        }
+    }
+
+}
