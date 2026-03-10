a4e2e4681bb31c76db89d1ebe154546a7f6f46de
kc357174 <kc357174@gmail.com>
2026-03-10 21:17:33 +0530

    Initial commit

---
+++ AI-Interview-Sys/src/main/java/com/example/AI_Interview_Sys/repository/InterviewRepository.java
@@ -0,0 +1,11 @@
+package com.example.AI_Interview_Sys.repository;
+
+
+import com.example.AI_Interview_Sys.model.Interview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
+
+}
+
+
