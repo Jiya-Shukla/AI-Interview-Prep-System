5c5354f2c331094eda0e3691220d0d9b06adec77
Jiya Shukla <jiya.shukla01012005@gmail.com>
2026-04-03 21:58:42 +0530

    Delete src directory

--- src/main/java/com/example/AI_Interview_Sys/repository/InterviewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.AI_Interview_Sys.repository;
-
-import com.example.AI_Interview_Sys.model.Interview;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterviewRepository extends JpaRepository<Interview, Long> {
-
-}
